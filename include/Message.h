0d46e97e711f7f0b6403ff00a3f51a82d6546234
shutian.lzh <shutian.lzh@alibaba-inc.com>
2018-02-24 15:15:55 +0800

    Add doc to Message interface

--- include/Message.h
+++ include/Message.h
@@ -9,33 +9,57 @@
 #include "Namespace.h"
 
 BEGIN_NAMESPACE_2(io, openmessaging)
-        class Message {
-        public:
-            virtual ~Message() {
 
-            }
+    /**
+     * The {@code Message} interface is the root interface of all OMS messages, and the most commonly used OMS message is
+     * {@link BytesMessage}.
+     * <p>
+     * Most message-oriented middleware (MOM) products treat messages as lightweight entities that consist of a header and a
+     * body, like <a href="http://rocketmq.apache.org/">Apache RocketMQ</a>. The header part contains fields used for message
+     * routing and identification; the body part contains the application data to sent.
+     * <p>
+     * The {@code Message} is a lightweight entity that only contains properties related to information of a specific message
+     * object, and the {@code Message} is composed of the following parts:
+     *
+     * <UL>
+     * <LI>Header - All messages support the same set of header fields. Header fields contain values used by both
+     * clients and providers to identify and route messages.
+     * <LI>Properties - Each message contains a built-in facility for supporting application-defined property values.
+     * Properties provide an efficient mechanism for supporting application-defined message filtering.
+     * </UL>
+     *
+     * The body part is deferred to the sub-classes of {@code Message}.
+     *
+     * @version OMS 1.0
+     * @since OMS 1.0
+     */
+    class Message {
+    public:
+        virtual ~Message() {
 
-            virtual boost::shared_ptr<KeyValue> sysHeaders() = 0;
+        }
 
-            virtual boost::shared_ptr<KeyValue> userHeaders() = 0;
+        virtual boost::shared_ptr<KeyValue> sysHeaders() = 0;
 
-            virtual Message& putSysHeaders(const std::string &key, int value) = 0;
+        virtual boost::shared_ptr<KeyValue> userHeaders() = 0;
 
-            virtual Message& putSysHeaders(const std::string &key, long value) = 0;
+        virtual Message& putSysHeaders(const std::string &key, int value) = 0;
 
-            virtual Message& putSysHeaders(const std::string &key, double value) = 0;
+        virtual Message& putSysHeaders(const std::string &key, long value) = 0;
 
-            virtual Message& putSysHeaders(const std::string &key, const std::string &value) = 0;
+        virtual Message& putSysHeaders(const std::string &key, double value) = 0;
 
-            virtual Message& putUserHeaders(const std::string &key, int value) = 0;
+        virtual Message& putSysHeaders(const std::string &key, const std::string &value) = 0;
 
-            virtual Message& putUserHeaders(const std::string &key, long value) = 0;
+        virtual Message& putUserHeaders(const std::string &key, int value) = 0;
 
-            virtual Message& putUserHeaders(const std::string &key, double value) = 0;
+        virtual Message& putUserHeaders(const std::string &key, long value) = 0;
 
-            virtual Message& putUserHeaders(const std::string &key, const std::string &value) = 0;
+        virtual Message& putUserHeaders(const std::string &key, double value) = 0;
 
-        };
+        virtual Message& putUserHeaders(const std::string &key, const std::string &value) = 0;
+
+    };
 
 END_NAMESPACE_2(io, openmessaging)
 

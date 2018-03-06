9fbca1f9672a410661225edc13715cb81d39c315
shutian.lzh <shutian.lzh@alibaba-inc.com>
2018-03-06 10:48:41 +0800

    Put tests in namespace

--- tests/OMSTest.cpp
+++ tests/OMSTest.cpp
@@ -4,22 +4,24 @@
 #include "OMS.h"
 #include "KeyValueImpl.h"
 
-class OMSTest : public ::testing::Test {
-public:
-    virtual void SetUp() {
-        io::openmessaging::Initialize();
+BEGIN_NAMESPACE_2(io, openmessaging)
+    class OMSTest : public ::testing::Test {
+    public:
+        virtual void SetUp() {
+            io::openmessaging::Initialize();
+        }
+    };
+
+    TEST_F(OMSTest, testNewKeyValue) {
+        using namespace io::openmessaging;
+
+        boost::shared_ptr<KeyValue> kv = OMS::newKeyValue();
+        ASSERT_TRUE(kv);
+
+        boost::shared_ptr<KeyValueImpl> impl = boost::dynamic_pointer_cast<KeyValueImpl>(kv);
+        ASSERT_TRUE(impl);
+
+        jobject  proxy = impl->getProxy();
+        ASSERT_TRUE(proxy);
     }
-};
-
-TEST_F(OMSTest, testNewKeyValue) {
-    using namespace io::openmessaging;
-
-    boost::shared_ptr<KeyValue> kv = OMS::newKeyValue();
-    ASSERT_TRUE(kv);
-
-    boost::shared_ptr<KeyValueImpl> impl = boost::dynamic_pointer_cast<KeyValueImpl>(kv);
-    ASSERT_TRUE(impl);
-
-    jobject  proxy = impl->getProxy();
-    ASSERT_TRUE(proxy);
-}
+END_NAMESPACE_2(io, openmessaging)

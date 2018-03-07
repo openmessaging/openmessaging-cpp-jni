c42cd8d3d15b03da684a7a39431398c476de9802
shutian.lzh <shutian.lzh@alibaba-inc.com>
2018-03-07 14:20:00 +0800

    Add more examples

---
+++ example/PushConsumer.cpp
@@ -0,0 +1,73 @@
+#include <boost/smart_ptr.hpp>
+#include <boost/lexical_cast.hpp>
+
+#include <plog/Log.h>
+
+#include <MessagingAccessPointFactory.h>
+#include "core.h"
+#include "NonStandardKeys.h"
+
+BEGIN_NAMESPACE_3(io, openmessaging, consumer)
+
+    class ExampleMessageListener : virtual public MessageListener {
+    public:
+
+        virtual void onMessage(boost::shared_ptr<Message> &message, boost::shared_ptr<Context> &context) {
+            boost::shared_ptr<KeyValue> sysHeaders = message->sysHeaders();
+            std::string msgId = sysHeaders->getString(BuiltinKeys::MessageId);
+            LOG_INFO << "A new message received. MsgId: " << msgId;
+        }
+    };
+
+END_NAMESPACE_3(io, openmessaging, consumer)
+
+using namespace std;
+using namespace io::openmessaging;
+using namespace io::openmessaging::consumer;
+
+int main(int argc, char *argv[]) {
+
+    int count = 128;
+
+    if (argc > 1) {
+        count = boost::lexical_cast<int>(argv[1]);
+    }
+
+    // Define access point
+    const string accessPointUrl = "oms:rocketmq://ons3.dev:9876/default:default";
+
+    // Define implementation driver class key
+    const string driverClassKey = "oms.driver.impl";
+
+    // Define implementation driver class
+    const string driverClass = "io.openmessaging.rocketmq.MessagingAccessPointImpl";
+
+    // Initialize SDK
+    Initialize();
+
+    // Create Key-Value container to hold custom settings
+    boost::shared_ptr<KeyValue> kv = OMS::newKeyValue();
+
+    // Configure driver class
+    kv->put(driverClassKey, driverClass);
+
+    // Acquire messaging access point instance through factory method
+    boost::shared_ptr<MessagingAccessPoint> accessPoint = MessagingAccessPointFactory::getMessagingAccessPoint(accessPointUrl, kv);
+
+    std::string queueName("TopicTest");
+
+    boost::shared_ptr<KeyValue> subKV = OMS::newKeyValue();
+    const std::string value = "OMS_CONSUMER";
+    subKV->put(NonStandardKeys::CONSUMER_GROUP, value);
+
+    boost::shared_ptr<consumer::PushConsumer> pushConsumer = accessPoint->createPushConsumer(subKV);
+    boost::shared_ptr<MessageListener> messageListener = boost::make_shared<ExampleMessageListener>();
+    pushConsumer->attachQueue(queueName, messageListener);
+
+    pushConsumer->startup();
+
+    LOG_INFO << "Push consumer starts OK";
+
+    sleep(60 * 60);
+
+}

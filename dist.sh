a7f14d0eda08e2cd51dec56017d00fb448986ddf
Zhanhui Li <lizhanhui@apache.org>
2018-03-17 23:43:52 +0800

    Add messages showing current progress

--- dist.sh
+++ dist.sh
@@ -5,29 +5,44 @@ function clean() {
         rm -fr dist
     fi
 
+    if test -f dist.tar.gz; then
+        rm -f dist.tar.gz
+        echo "Previous artifact dist.tar.gz is removed"
+    fi
+
     mkdir dist
 }
 
 function copy_headers() {
+    echo "Copying header files"
     cp -r include dist/
+    echo "Done"
 }
 
 function copy_shared_libraries() {
+    echo "Copying shared libraries"
     cp -r build/lib/* dist/
+    echo "Done"
 }
 
 function copy_examples() {
+    echo "Copying examples"
     cp -r example dist/
     rm -f dist/example/CMakeLists.txt
+    echo "Done"
 }
 
 function copy_vm() {
+    echo "Copying VirtualMachine"
     cp -r third_party/jdk8m dist/vm
     rm -fr dist/vm/include
+    echo "Done"
 }
 
 function copy_vendors() {
+    echo "Copying OMS vendor implementations"
     mkdir -p dist/vendors/rocketmq
+    echo "Done"
 }
 
 function copy_artifacts() {

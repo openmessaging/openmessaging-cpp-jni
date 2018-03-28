ddebda178c70ab21872515002b660d0552758b5e
shutian.lzh <shutian.lzh@alibaba-inc.com>
2018-03-28 17:58:06 +0800

    Implement custom shared_ptr

--- include/smart_pointer.h
+++ include/smart_pointer.h
@@ -13,6 +13,145 @@
 
 #define NS boost
 
+#include "Namespace.h"
+
+BEGIN_NAMESPACE_2(io, openmessaging)
+        class atomic_int {
+        public:
+            atomic_int() : value_(0) {
+            }
+
+            atomic_int(int value) : value_(value) {
+            }
+
+            atomic_int(const atomic_int &other) {
+                value_ = other.value_;
+            }
+
+            atomic_int &operator=(const atomic_int &other) {
+                value_ = other.value_;
+                return *this;
+            }
+
+            // prefix
+            atomic_int &operator++() {
+                __sync_fetch_and_add(&value_, 1);
+                return *this;
+            }
+
+            // postfix
+            atomic_int &operator++(int) {
+                __sync_fetch_and_add(&value_, 1);
+                return *this;
+            }
+
+            // prefix
+            atomic_int &operator--() {
+                __sync_fetch_and_sub(&value_, 1);
+                return *this;
+            }
+
+            // postfix
+            atomic_int &operator--(int) {
+                __sync_fetch_and_sub(&value_, 1);
+                return *this;
+            }
+
+            int get() {
+                return value_;
+            }
+
+        private:
+            int value_;
+        };
+
+        template<typename T>
+        class shared_ptr {
+        public:
+            shared_ptr() : ptr_(nullptr), refCount(nullptr) {}
+
+            shared_ptr(T *ptr) : ptr_(ptr), refCount(new atomic_int(1)) {
+
+            }
+
+            shared_ptr(const shared_ptr &other) {
+                refCount = other.refCount;
+                if (refCount) {
+                    (*refCount)++;
+                }
+            }
+
+            ~shared_ptr() {
+                if (refCount) {
+                    (*refCount)--;
+                    if (refCount->get() == 0) {
+                        delete(ptr_);
+                        delete(refCount);
+                    }
+                }
+            }
+
+            shared_ptr& operator=(const shared_ptr &other) {
+                if (this == &other) {
+                    return *this;
+                }
+
+                if (refCount) {
+                    (*refCount)--;
+                    if (refCount->get() == 0) {
+                        delete (ptr_);
+                        delete(refCount);
+                    }
+                }
+                ptr_ = other.ptr_;
+                refCount = other.refCount;
+                if (refCount) {
+                    (*refCount)++;
+                }
+
+                return *this;
+            }
+
+            T& operator*() {
+                return *ptr_;
+            }
+
+            T* operator->() {
+                return ptr_;
+            }
+
+            T* get() {
+                return ptr_;
+            }
+
+        private:
+            T *ptr_;
+            atomic_int *refCount;
+        };
+
+    template <typename T>
+    shared_ptr<T> make_shared() {
+        T* p = new T();
+        return shared_ptr<T>(p);
+    }
+
+    template <typename T, typename Arg>
+    shared_ptr<T> make_shared(Arg arg) {
+        T* p = NULL;
+
+        try {
+            p = new T(arg);
+        } catch (...) {
+            //TODO: allocate memory failed.
+            delete p;
+            return shared_ptr<T>();
+        }
+
+        return shared_ptr<T>(p);
+    }
+
+END_NAMESPACE_2(io, openmessaging)
+
 #endif
 
 
---
+++ tests/smart_pointer_test.cpp
@@ -0,0 +1,53 @@
+#include <gtest/gtest.h>
+#include "smart_pointer.h"
+#include <iostream>
+
+using namespace io::openmessaging;
+
+class Type {
+public:
+    Type() {
+        std::cout << "Ctor" << std::endl;
+    }
+
+    ~Type() {
+        std::cout << "Dtor" << std::endl;
+    }
+
+    void execute() {
+        std::cout << "Dummy function" << std::endl;
+    }
+};
+
+TEST(smart_pointer_test, test_ctor_1) {
+
+    Type *t = new Type;
+    {
+        shared_ptr<Type> ptr(t);
+        ptr->execute();
+    }
+
+    std::cout << "Should have destructed" << std::endl;
+}
+
+TEST(smart_pointer_test, test_ctor_2) {
+
+    Type *t = new Type;
+    shared_ptr<Type> p2;
+    {
+        shared_ptr<Type> ptr(t);
+
+        p2 = ptr;
+        p2->execute();
+    }
+}
+
+TEST(smart_pointer_test, test_make_shared) {
+    shared_ptr<Type> ptr = make_shared<Type>();
+    ptr->execute();
+}
+
+TEST(smart_pointer_test, test_make_shared_2) {
+    shared_ptr<int> ptr = make_shared<int>(1);
+    std::cout << *ptr << std::endl;
+}

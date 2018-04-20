318d8f9da44cc01d312fe946f0e4001aa6bc848b
shutian.lzh <shutian.lzh@alibaba-inc.com>
2018-04-20 16:46:44 +0800

    Verbose output of creating directories

--- install_oms_impl.sh
+++ install_oms_impl.sh
@@ -2,12 +2,7 @@
 OMS_IMPL_DIR="/usr/local/lib/oms"
 
 if [ ! -d $OMS_IMPL_DIR/vendor ]; then
-    mkdir -p $OMS_IMPL_DIR
-
-    if [ ! -d $OMS_IMPL_DIR ]; then
-        echo "Create directory $OMS_IMPL_DIR failed"
-    fi
-
+    mkdir -v -p $OMS_IMPL_DIR
     wget -O vendor.tar.gz http://shutian.oss-cn-hangzhou.aliyuncs.com/vendor.tar.gz
     tar -xzf vendor.tar.gz -C /usr/local/lib/oms
 fi

86895b825a048d28daf644de79924cb230fe4b6c
shutian.lzh <shutian.lzh@alibaba-inc.com>
2018-04-20 14:41:37 +0800

    Remove sudo as travis does not allow sudo on default considering it's using docker as build infrastructure

--- install_boost.sh
+++ install_boost.sh
@@ -3,4 +3,4 @@ wget -O ${HOME}/boost_1_67_0.tar.gz https://dl.bintray.com/boostorg/release/1.67
 tar -xzvf ${HOME}/boost_1_67_0.tar.gz -C ${HOME}
 cd ${HOME}/boost_1_67_0
 ./bootstrap.sh --prefix=/usr/local --with-libraries=system,filesystem,chrono,thread,date_time
-sudo ./bjam cflags="-fPIC" cxxflags="-fPIC" link=static --with-system --with-thread --with-date_time --with-filesystem --with-chrono  -a install
+./bjam cflags="-fPIC" cxxflags="-fPIC" link=static --with-system --with-thread --with-date_time --with-filesystem --with-chrono  -a install

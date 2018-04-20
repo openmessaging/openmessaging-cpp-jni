4cbead68ada2fe6489f3705979d21443e7383c0f
shutian.lzh <shutian.lzh@alibaba-inc.com>
2018-04-20 15:19:21 +0800

    Make build log less verbose

--- install_boost.sh
+++ install_boost.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-wget -O ${HOME}/boost_1_67_0.tar.gz https://dl.bintray.com/boostorg/release/1.67.0/source/boost_1_67_0.tar.gz
-tar -xzf ${HOME}/boost_1_67_0.tar.gz -C ${HOME}
-cd ${HOME}/boost_1_67_0
+
+BOOST=boost_1_67_0
+
+if [ ! -d ${HOME}/${BOOST} ]; then
+    wget -O ${HOME}/boost_1_67_0.tar.gz https://dl.bintray.com/boostorg/release/1.67.0/source/boost_1_67_0.tar.gz
+    tar -xzf ${HOME}/boost_1_67_0.tar.gz -C ${HOME}
+fi
+
+cd ${HOME}/${BOOST}
+
 ./bootstrap.sh --prefix=/usr/local --with-libraries=system,filesystem,chrono,thread,date_time
-./bjam cflags="-fPIC" cxxflags="-fPIC" link=static --with-system --with-thread --with-date_time --with-filesystem --with-chrono  -a install
+
+./bjam cflags="-fPIC" cxxflags="-fPIC" link=static \
+       --with-system --with-thread --with-date_time --with-filesystem --with-chrono  \
+       -a install \
+       > ${HOME}/build.log

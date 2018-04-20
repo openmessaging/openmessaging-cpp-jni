7b0117b2db690cf02f7c59b7961fdae212e14181
shutian.lzh <shutian.lzh@alibaba-inc.com>
2018-04-20 14:59:03 +0800

    Make extraction quiet

--- install_boost.sh
+++ install_boost.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 wget -O ${HOME}/boost_1_67_0.tar.gz https://dl.bintray.com/boostorg/release/1.67.0/source/boost_1_67_0.tar.gz
-tar -xzvf ${HOME}/boost_1_67_0.tar.gz -C ${HOME}
+tar -xzf ${HOME}/boost_1_67_0.tar.gz -C ${HOME}
 cd ${HOME}/boost_1_67_0
 ./bootstrap.sh --prefix=/usr/local --with-libraries=system,filesystem,chrono,thread,date_time
 ./bjam cflags="-fPIC" cxxflags="-fPIC" link=static --with-system --with-thread --with-date_time --with-filesystem --with-chrono  -a install
